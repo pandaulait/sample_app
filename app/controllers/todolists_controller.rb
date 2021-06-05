32c34821fbe7affb8f8f957869079b8f70957c59
pandaulait <show.hey.panda@gmail.com>
2021-06-05 07:20:48 +0000

    [FInish]5-8

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -12,18 +12,26 @@ class TodolistsController < ApplicationController
     redirect_to todolist_path(list.id)
   end
 
-    def index
-      @lists = List.all
-    end
+  def index
+    @lists = List.all
+  end
 
-    def show
-      @list=List.find(params[:id])
-    end
+  def show
+    @list=List.find(params[:id])
+  end
+  def edit
+    @list = List.find(params[:id])
+  end
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+  end
 
 
   private
   #ストロングパラメーター
   def list_params
-    params.require(:list).permit(:title,:body)
+    params.require(:list).permit(:title,:body,:image)
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '62d9735d9e8d6e1c92ae0f91953edaef2fb3f89aae4ee40e9114e2c165e7c16efa13c22f1cbc8540ea4821f74053ae59a746a721676f0bacd4aa5acd37fa4426'
